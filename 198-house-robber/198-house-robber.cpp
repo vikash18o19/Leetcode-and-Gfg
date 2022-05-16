57cfb1e2c031436d4ff41dc22b6a5ca33ceb0296
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-05-16 15:05:09 +0530

    Time: 4 ms (33.43%), Space: 7.8 MB (48.66%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int collect(vector<int> &nums,int ind, vector<int> &dp){
-        if(ind>=nums.size()){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int take = nums[ind] + collect(nums,ind+2,dp);
-        int not_take = collect(nums,ind+1,dp);
+//     int collect(vector<int> &nums,int ind, vector<int> &dp){
+//         if(ind>=nums.size()){
+//             return 0;
+//         }
+//         if(dp[ind]!=-1){
+//             return dp[ind];
+//         }
+//         int take = nums[ind] + collect(nums,ind+2,dp);
+//         int not_take = collect(nums,ind+1,dp);
         
-        return(dp[ind] = max(take,not_take));
+//         return(dp[ind] = max(take,not_take));
          
-    }
+//     }
     int rob(vector<int>& nums) {
-        int ans;
-        vector<int> dp(nums.size(),-1);
-        ans = collect(nums,0,dp);
-        return ans;
+        //int ans;
+        vector<int> dp(nums.size()+2,-1);
+        //ans = collect(nums,0,dp);
+        
+        dp[nums.size()]=0;
+        dp[nums.size()+1]=0;
+        for(int ind = nums.size()-1;ind>=0;ind--){
+            int take = nums[ind] + dp[ind+2];
+            int not_take = dp[ind+1];
+        
+            dp[ind] = max(take,not_take);
+        }
+        return dp[0];
         
         
     }
