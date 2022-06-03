40b1180b41e2aefa0c1bc65f00bb9c817f20934b
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-03 16:47:44 +0530

    Time: 355 ms (15.48%), Space: 140.8 MB (22.13%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string sub(int l,int h, string s){
+        string ans="";
+        while(l<=h){
+            ans+=s[l];
+            l++;
+        }
+        
+        return ans;
+    }
+    bool palin(int l, int h, string &s){
+        while(l<h){
+            if(s[l]!=s[h]){
+                return false;
+            }
+            l++;
+            h--;
+        }
+        return true;
+    }
+    void solve(string &s, int ind, int n, vector<string> ans, 
+               vector<vector<string>> &out){
+        if(ind == n){
+            out.push_back(ans);
+            return;
+        }
+        
+        for(int i = ind; i<n; i++){
+            if(palin(ind,i,s)){
+                ans.push_back(sub(ind,i,s));
+                //cout<<"pushed: "<<sub(ind,i,s)<<" with ind: "<<ind<<" and i: "<<i<<"\n";
+                solve(s,i+1,n,ans,out);
+                ans.erase(ans.end()-1);
+            }
+        }
+        return;
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> out;
+        vector<string> ans;
+        int n = s.size();
+        solve(s,0,n,ans,out);
+        //cout<<"test: "<<s.substr(1,1+1);
+        return out;
+    }
+};
