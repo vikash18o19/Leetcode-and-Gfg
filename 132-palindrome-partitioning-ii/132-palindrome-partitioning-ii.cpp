166802691d11ad896d538ca8f6a8b737ac6e407a
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-09 15:35:57 +0530

    Time: 541 ms (26.97%), Space: 15.8 MB (52.57%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -11,28 +11,20 @@ public:
         }
         return true;
     }
-    int solve(string &s, int ind, int n,vector<int> &dp){
-        
-        if(ind == s.size()){
-            return 0;
-        }
-         if(dp[ind]!=-1){
-             return dp[ind];
-         }
-        priority_queue <int, vector<int>, greater<int>> pq;
-        for(int i = ind; i<n; i++){
-            if(palin(ind,i,s)){
-                
-                pq.push(1 + solve(s,i+1,n,dp));
-                
-            }
-        }
-        return dp[ind]=pq.top();
-    }
     int minCut(string s) {
-        vector<int> dp(s.size(),-1);
+        vector<int> dp(s.size()+1,0);
         int n = s.size();
-        int out = solve(s,0,n,dp);
-        return out-1;
+        for(int ind = n-1;ind>=0;ind--){
+            priority_queue <int, vector<int>, greater<int>> pq;
+            for(int i = ind; i<n; i++){
+                if(palin(ind,i,s)){
+
+                    pq.push(1 + dp[i+1]);
+
+                }
+            }
+            dp[ind]=pq.top();
+        }
+        return dp[0]-1;
     }
 };
