b13160e578125f3cb7753e82c04e8c6a83082091
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-09 15:59:32 +0530

    Time: 21 ms (38.57%), Space: 10.9 MB (26.86%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int ind,vector<int> &nums, vector<vector<int>> &ans){
+        if(ind==nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        unordered_set<int> s;
+        for(int i=ind;i<nums.size();i++){
+            if(s.find(nums[i])!=s.end()) continue;
+            s.insert(nums[i]);
+            swap(nums[ind],nums[i]);
+            solve(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
+        return;
+        
+        
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
