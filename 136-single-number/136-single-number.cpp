4db3bd8988d3bfbc9d2f0a5ca33512a011fce9a6
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-20 13:52:09 +0530

    Time: 48 ms (16.22%), Space: 17 MB (52.48%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int temp;
+        int count=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(count==0){
+                temp=nums[i];
+                count++;
+            }
+            else if(count<2){
+                if(temp==nums[i]) count++;
+                else return temp;
+            }
+            if(count==2) count=0;
+        }
+        
+        return temp;
+    }
+};
