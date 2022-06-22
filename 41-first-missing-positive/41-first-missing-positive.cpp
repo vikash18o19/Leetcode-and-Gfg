d834c66591d0ff01bf4365dac2052f9d1007a1fe
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-22 23:28:27 +0530

    Time: 239 ms (49.81%), Space: 83 MB (77.34%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        
         int n = nums.size();
-        int ans=-1;
+        int ans = -1;
         int flag = 0;
+        
         for(int i = 0;i<n;i++){
             if(nums[i]==1){
                 flag=1;
@@ -12,32 +14,29 @@ public:
                 nums[i]=1;
             }
         }
-        // for(int i = 0;i<n;i++){
-        //     cout<<nums[i]<<" "; 
-        // }
+        
         if(flag==0){
             return 1;
         }
+        
         for(int i =0;i<n;i++){
             if(nums[abs(nums[i])-1]>0){
                 nums[abs(nums[i])-1]=-1*nums[abs(nums[i])-1];
             }
         }
-        cout<<"\n";
-        // for(int i = 0;i<n;i++){
-        //     cout<<nums[i]<<" "; 
-        // }
-        //cout<<nums[0]<<" "<<ans;
-        
+    
         for(int i =0;i<n;i++){
             if(nums[i]>0){
                 ans=i+1;
                 break;
             }
         }
+        
         if(ans==-1){
             ans=n+1;
         }
+        
         return ans;
+        
     }
 };
