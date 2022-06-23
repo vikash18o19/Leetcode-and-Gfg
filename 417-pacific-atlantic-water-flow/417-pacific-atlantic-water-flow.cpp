4490c95ca0a7faa83a477e9f9eabef20d272952c
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-23 20:52:10 +0530

    Time: 85 ms (31.64%), Space: 19 MB (50.17%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
+    
+    // reverse approach intead of finding from where both oceans can be reached 
+    // finding at what positions each ocean can reach.
+    
     void fill(vector<vector<int>> &h,int i, int j,vector<vector<int>> &pac,int &m, int &n,int prev){
+        // edge cases.
         if(i<0 || j<0 || i>=m || j>=n){
             return;
         }
         
+        //if cell is already visited.
         if(pac[i][j]==1) return;
         
-        if(h[i][j]>=prev){
+        
+        if(h[i][j]>=prev){//if cell is explorable.
             pac[i][j]=1;
             fill(h,i-1,j,pac,m,n,h[i][j]);
             fill(h,i,j+1,pac,m,n,h[i][j]);
@@ -22,6 +29,8 @@ public:
         int n = h[0].size();
         vector<vector<int>> pac(m,vector<int>(n,0));
         vector<vector<int>> atl(m,vector<int>(n,0));
+        
+        //starting from edge of both the oceans.
         for(int j =0;j<n;j++){
             if(pac[0][j]==1) continue;
             fill(h,0,j,pac,m,n,INT_MIN);
@@ -41,10 +50,11 @@ public:
         }
         vector<vector<int>> ans;
         vector<int>temp(2,-1);
+        
+        // finding where both atlantic and pacific water can reach.
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(atl[i][j]&&pac[i][j]){
-                    //cout<<i<<" "<<j<<"   ";
                     temp[0]=i;
                     temp[1]=j;
                     ans.push_back(temp);
