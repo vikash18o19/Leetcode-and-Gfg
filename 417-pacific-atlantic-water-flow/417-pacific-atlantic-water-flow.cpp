50658ca650a4acc762c30279181e37349cc5a699
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-06-23 20:16:56 +0530

    Time: 90 ms (27.90%), Space: 19 MB (50.17%) - LeetHub

---
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void fill(vector<vector<int>> &h,int i, int j,vector<vector<int>> &pac,int &m, int &n,int prev){
+        if(i<0 || j<0 || i>=m || j>=n){
+            return;
+        }
+        
+        if(pac[i][j]==1) return;
+        
+        if(h[i][j]>=prev){
+            pac[i][j]=1;
+            fill(h,i-1,j,pac,m,n,h[i][j]);
+            fill(h,i,j+1,pac,m,n,h[i][j]);
+            fill(h,i+1,j,pac,m,n,h[i][j]);
+            fill(h,i,j-1,pac,m,n,h[i][j]);
+        }
+        
+        return;
+    }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& h) {
+        int m = h.size();
+        int n = h[0].size();
+        vector<vector<int>> pac(m,vector<int>(n,0));
+        vector<vector<int>> atl(m,vector<int>(n,0));
+        for(int j =0;j<n;j++){
+            if(pac[0][j]==1) continue;
+            fill(h,0,j,pac,m,n,INT_MIN);
+        }
+        for(int i =0;i<m;i++){
+            if(pac[i][0]==1) continue;
+            fill(h,i,0,pac,m,n,INT_MIN);
+        }
+        
+        for(int j =0;j<n;j++){
+            if(atl[m-1][j]==1) continue;
+            fill(h,m-1,j,atl,m,n,INT_MIN);
+        }
+        for(int i =0;i<m;i++){
+            if(atl[i][n-1]==1) continue;
+            fill(h,i,n-1,atl,m,n,INT_MIN);
+        }
+        vector<vector<int>> ans;
+        vector<int>temp(2,-1);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(atl[i][j]&&pac[i][j]){
+                    //cout<<i<<" "<<j<<"   ";
+                    temp[0]=i;
+                    temp[1]=j;
+                    ans.push_back(temp);
+                }
+            }
+        }
+        return ans;
+    }
+};
