4cfaa8e5dd1eef1b8da5fa06c52b9d1a12677016
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-07-12 04:47:18 +0530

    Time: 339 ms (14.06%), Space: 47.3 MB (8.02%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    
-    int solve(int e,int f,vector<vector<int>> &dp){
-        // Base case(if there are 0 floors or 1 floor then minimum attempts can be floor size)
-        if(f==0 || f==1)
-            return f;
-        // if there is only 1 given egg then the worst case is that it will break from top
-        if(e==1)
-            return f;
-        // Memoization usage if element found in dp matrix return that
-        if(dp[e][f]!=-1)
-            return dp[e][f];
-        // Declaring variables for binary search
-        int mini=INT_MAX,l=1,r=f;
-        // Binary search algo to optimize the code as it is giving TLE without using it
+    int solve(int k, int n, vector<vector<int>> &dp){
+        
+        if(n==1 || n==0){
+            return n;
+        }
+        
+        if(k==1){
+            return n;
+        }
+        
+        if(dp[n][k]!=-1){
+            return dp[n][k];
+        }
+        
+         int mn = INT_MAX;
+        int l = 1;
+        int r = n;
         while(l<=r){
-            int mid=l+(r-l)/2;
-            int left=solve(e-1,mid-1,dp); // passed e-1 considering egg is breaked
-            int right=solve(e,f-mid,dp);// passed e considering egg is not breaked
-            int temp=1+max(left,right); // finding max bcz calculating in worst case
-            if(left<right)
+            int mid = l+ (r-l) /2;
+            int left = solve(k-1,mid-1,dp);
+            int right = solve(k,n-mid,dp);
+            int temp = 1 + max(left,right);
+            if (left<right)  
                 l=mid+1;
             else
                 r=mid-1;
-            mini=min(temp,mini);// Finding minimum number of attempts possible
+            mn = min(mn,temp);
+   
         }
-        return dp[e][f]=mini;
+        return dp[n][k]=mn;
     }
-    
     int superEggDrop(int k, int n) {
-        vector<vector<int>>dp(k+1,vector<int>(n+1,-1)); // initialising a 2-D dp matrix with -1 values
+         vector<vector<int>> dp(n+1,vector<int>(k+1,-1));
         return solve(k,n,dp);
     }
 };
