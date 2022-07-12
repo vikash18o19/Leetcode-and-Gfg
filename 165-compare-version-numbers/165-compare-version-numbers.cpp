eb828288538bc1616c600f317ff8f8579a9e02d1
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-07-12 22:45:36 +0530

    Time: 7 ms (5.21%), Space: 6.2 MB (29.31%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -14,17 +14,17 @@ public:
             v.push_back(stoi(temp));
             temp.clear();
         }
-        //stoi();  
     }
     int compareVersion(string version1, string version2) {
         vector<int> v1;
-        solve(version1,v1);
+        solve(version1,v1);//storing numeric values in v1.
         vector<int> v2;
-        solve(version2,v2);
+        solve(version2,v2);//storing numeric values in v2
         int n1 = v1.size();
         int n2 = v2.size();
         int n =abs(n1-n2);
-        for(int i =0;i<n;i++){
+        
+        for(int i =0;i<n;i++){ //making size of v1 and v2 equal by adding 0s.
             if(n1<n2){
                 v1.push_back(0);
             }
@@ -32,9 +32,8 @@ public:
                 v2.push_back(0);
             }
         }
-        //cout<<n;
-        for(int i =0;i<max(n1,n2);i++){
-            //cout<<v1[i]<<" "<<v2[i]<<"\n";
+        
+        for(int i =0;i<max(n1,n2);i++){//comparing from left to right to find which version is newer or are same.
             if(v1[i]<v2[i]){
                 return -1;
             }
