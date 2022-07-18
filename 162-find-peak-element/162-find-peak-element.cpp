d7bdc54c6d36079b51cff29356b7e4f6486f167f
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-07-18 21:34:43 +0530

    Time: 7 ms (50.83%), Space: 8.7 MB (99.41%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -7,37 +7,16 @@ public:
         if(n==1){
             return 0;
         }
-        while(l<=h){
+        while(l<h){
             //cout<<"l: "<<l<<" h: "<<h<<"\n";
             int mid = l+(h-l)/2;
-            //cout<<mid<<" \n";
-            if(mid==0 && nums[mid+1]<nums[mid]){
-                return mid;
-            }
-            if(mid==0 && nums[mid+1]>nums[mid]){
-                l=mid+1;
-                continue;
-            }
-            if(mid==n-1 && nums[mid-1]<nums[mid]){
-                return mid;
-            }
-            if(mid==n-1 && nums[mid-1]>nums[mid]){
-                h=mid-1;
-                continue;
-            }
-            if(nums[mid-1]<nums[mid] && nums[mid+1]<nums[mid]){
-                return mid;
-            }
-            else if(nums[mid]<nums[mid+1] && nums[mid-1]<nums[mid]){
-                l=mid+1;
-            }
-            else if(nums[mid]>nums[mid+1] && nums[mid-1]>nums[mid]){
-                h=mid-1;
+            if(nums[mid]>nums[mid+1]){
+                h=mid;
             }
             else{
                 l=mid+1;
             }
         }
-        return -1;
+        return l;
     }
 };
