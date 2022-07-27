8d6ae94331fe08482f7a211aaf802e07bea63502
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-07-28 01:33:49 +0530

    Time: 289 ms (66.67%), Space: 26.6 MB (100.00%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -9,23 +9,15 @@ public:
             row="*";
             col="*";
             for(int j =0;j<grid[0].size();j++){
-                
                 row+=to_string(grid[i][j])+"*";
                 col+=to_string(grid[j][i])+"*";
-                
             }
-            cout<<"row: "<<row<<"\n";
-            cout<<"col: "<<col<<"\n";
             r[row]++;
             c[col]++;
         }
-        cout<<"\n\n";
-        cout<<r.size()<<"\n";
         int count=0;
         for(auto i = r.begin();i!=r.end();i++){
-            cout<<"to find: "<<i->first<<"\n";
             if(c.find(i->first)!=c.end()){
-                cout<<"got it "<<"\n";
                 int temp = i->second*c.find(i->first)->second;
                 count=count+temp;
             }
