38eaefafbbeea8a1e26de4d3fc74bce275624372
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-08-01 00:40:19 +0530

    Time: 11 ms (68.28%), Space: 11.3 MB (5.52%) - LeetHub

---
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> ge  (m,vector<int>(n,0));
+        vector<vector<int>> gw  (m,vector<int>(n,0));
+        vector<vector<int>> gn  (m,vector<int>(n,0));
+        vector<vector<int>> gs  (m,vector<int>(n,0));
+        
+        for(int i =0;i<m;i++){
+            for(int j =1;j<n;j++){
+                gw[i][j]=max(grid[i][j-1],gw[i][j-1]);
+                ge[i][n-j-1]=max(grid[i][n-j],ge[i][n-j]);
+                gn[j][i]=max(grid[j-1][i],gn[j-1][i]);
+                gs[m-j-1][i]=max(grid[m-j][i],gs[m-j][i]);
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                int h = min(max(ge[i][j],gw[i][j]),
+                            max(gn[i][j],gs[i][j]));
+                int diff =  h-grid[i][j];
+                ans+=max(0,diff);
+            }
+        }
+        // for(int i =0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         cout<<ge[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        // cout<<"\n";
+        // for(int i =0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         cout<<gw[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        // cout<<"\n";
+        // for(int i =0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         cout<<gn[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        // cout<<"\n";
+        // for(int i =0;i<m;i++){
+        //     for(int j =0;j<n;j++){
+        //         cout<<gs[i][j]<<" ";
+        //     }
+        //     cout<<"\n";
+        // }
+        
+        return ans;
+    }
+};
