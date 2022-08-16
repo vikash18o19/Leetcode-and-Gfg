0eca466a86131a09abc4ef0128eacec07eccec8c
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-08-16 16:54:38 +0530

    Time: 19 ms (99.12%), Space: 13.3 MB (54.00%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     
     vector<int> findBall(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<int> ans(n, -1);
-        for(int i = 0;i<n;i++) {
-            int r = 0, c = i;
-            while(r < m) {
-                if(grid[r][c] == 1) {
-                    if(c == n-1) break; //it's a wall
-                    if(c < n-1 && grid[r][c+1] == -1) break; // it's V shaped
-                    r++;c++;
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<int> ans(n,-1);
+        for(int j=0;j<n;j++){
+            int r =0;
+            int c =j;
+            while(r<m){
+                if(grid[r][c]==1){
+                    if(c==n-1) break;
+                    if(grid[r][c+1]==-1){
+                        break;
+                    }
+                    r++;
+                    c++;
                 }
-                else {
-                    if (c==0) break;
-                    if(c > 0 && grid[r][c-1] == 1) break;
-                    r++;c--;
+                else{
+                    if(c==0) break;
+                    if(grid[r][c-1]==1){
+                        break;
+                    }
+                    r++;
+                    c--;
+                    }
                 }
-            }
-            if(r==m) ans[i] = c; // we have passed through all columns 
+            if(r==m) ans[j]=c;
         }
-        
         return ans;
     }
 };
