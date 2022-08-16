2f603a9f708509191f63ad17ecc3ad11fedb7889
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-08-16 15:47:39 +0530

    Time: 57 ms (12.47%), Space: 14.6 MB (5.57%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    void solve(vector<vector<int>>& grid,vector<vector<int>> &vis,vector<int> &ans,int i,int j,int n, int m){
+    int solve(vector<vector<int>>& grid,vector<vector<int>> &vis,vector<int> &ans,int i,int j,int n, int m){
         if(i==m){
             ans.push_back(j);
-            return;
+            return j;
+        }
+        if(vis[i][j]!=-1){
+            return vis[i][j];
         }
         if(j==0 && grid[i][j]==-1){
             ans.push_back(-1);
-            return;
+            return -1;
         }
         if(j==n-1 && grid[i][j]==1){
             ans.push_back(-1);
-            return;
+            return -1;
         }
         if(grid[i][j]==1 && j<n-1 && grid[i][j+1]==-1){
             ans.push_back(-1);
-            return;
+            return -1;
         }
         if(grid[i][j]==-1 && j>0 && grid[i][j-1]==1){
             ans.push_back(-1);
-            return;
+            return -1;
         }
         if(grid[i][j]==1){
-            solve(grid,vis,ans,i+1,j+1,n,m);
+            return vis[i][j]=solve(grid,vis,ans,i+1,j+1,n,m);
         }
         if(grid[i][j]==-1){
-            solve(grid,vis,ans,i+1,j-1,n,m);
+            return vis[i][j]=solve(grid,vis,ans,i+1,j-1,n,m);
         }
+        return 1;
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         int m=grid.size();
@@ -34,7 +38,7 @@ public:
         vector<vector<int>> vis(m,vector<int>(n,-1));
         vector<int> ans;
         for(int j=0;j<n;j++){
-            solve(grid,vis,ans,0,j,n,m);
+            int x=solve(grid,vis,ans,0,j,n,m);
         }
         return ans;
     }
