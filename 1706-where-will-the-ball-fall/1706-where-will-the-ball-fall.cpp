dcaf02ff0462396e4eb6f439d5bc023827943c0e
Vikash <58856727+vikash18o19@users.noreply.github.com>
2022-08-16 16:06:04 +0530

    Time: 573 ms (5.17%), Space: 14.4 MB (5.87%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    int solve(vector<vector<int>>& grid,vector<vector<int>> &vis,vector<int> &ans,int i,int j,int n, int m){
+    void solve(vector<vector<int>>& grid,vector<vector<int>> &vis,vector<int> &ans,int i,int j,int n, int m){
         if(i==m){
             ans.push_back(j);
-            return j;
+            return;
         }
         if(vis[i][j]!=-1){
-            return vis[i][j];
+            ans.push_back(vis[i][j]);
         }
         if(j==0 && grid[i][j]==-1){
             ans.push_back(-1);
-            return -1;
+            return;
         }
         if(j==n-1 && grid[i][j]==1){
             ans.push_back(-1);
-            return -1;
+            return;
         }
         if(grid[i][j]==1 && j<n-1 && grid[i][j+1]==-1){
             ans.push_back(-1);
-            return -1;
+            return;
         }
         if(grid[i][j]==-1 && j>0 && grid[i][j-1]==1){
             ans.push_back(-1);
-            return -1;
+            return;
         }
         if(grid[i][j]==1){
-            return vis[i][j]=solve(grid,vis,ans,i+1,j+1,n,m);
+            solve(grid,vis,ans,i+1,j+1,n,m);
         }
-        if(grid[i][j]==-1){
-            return vis[i][j]=solve(grid,vis,ans,i+1,j-1,n,m);
+        else{
+            solve(grid,vis,ans,i+1,j-1,n,m);
         }
-        return 1;
+        return;
     }
     vector<int> findBall(vector<vector<int>>& grid) {
         int m=grid.size();
@@ -38,7 +38,13 @@ public:
         vector<vector<int>> vis(m,vector<int>(n,-1));
         vector<int> ans;
         for(int j=0;j<n;j++){
-            int x=solve(grid,vis,ans,0,j,n,m);
+            solve(grid,vis,ans,0,j,n,m);
+        }
+        for(int i =0;i<m;i++){
+            for(int j =0;j<n;j++){
+                cout<<i<<" "<<j<<": " <<vis[i][j]<<" | ";
+            }
+            cout<<"\n";
         }
         return ans;
     }
